a22930732786c134099af21312a201cff0b1e524
Carey Metcalfe <carey@cmetcalfe.ca>
2015-12-24 16:36:15 -0500

    Fix incompatibility with Entrust ST v3.1.0.32
    Fixes #1

--- app/src/main/java/ca/pr0ps/xposed/entrustunblocker/Unblocker.java
+++ app/src/main/java/ca/pr0ps/xposed/entrustunblocker/Unblocker.java
@@ -2,24 +2,42 @@ package ca.pr0ps.xposed.entrustunblocker;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 
 public class Unblocker implements IXposedHookLoadPackage {
 
-    @Override
-    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-        if (!lpparam.packageName.equals("com.entrust.identityGuard.mobile"))
-            return;
+  @Override
+  public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+    if (!lpparam.packageName.equals("com.entrust.identityGuard.mobile"))
+      return;
 
-        findAndHookMethod("com.entrust.identityGuard.mobile.Splash", lpparam.classLoader, "isDeviceSecure", new XC_MethodReplacement() {
-            @Override
-            protected Object replaceHookedMethod(MethodHookParam methodHookParam) throws Throwable {
-            // Replace the method and don't do anything.
-            // This method usually just sets the global isRooted variable.
-            return false;
-            }
-        });
+    //Before 2015-12-15
+    try {
+      findAndHookMethod("com.entrust.identityGuard.mobile.Splash", lpparam.classLoader, "isDeviceSecure", new XC_MethodReplacement() {
+        @Override
+        protected Object replaceHookedMethod(MethodHookParam methodHookParam) throws Throwable {
+          return false;
+        }
+      });
+      return;
     }
+    catch (NoSuchMethodError e){}
+
+    //v3.1.0.32 - Released on 2015-12-15
+    try {
+      findAndHookMethod("com.entrust.identityGuard.mobile.sdk.PlatformDelegate", lpparam.classLoader, "isDeviceSecure", new XC_MethodReplacement() {
+        @Override
+        protected Object replaceHookedMethod(MethodHookParam methodHookParam) throws Throwable {
+          return false;
+        }
+      });
+      return;
+    }
+    catch (NoSuchMethodError e){}
+
+    XposedBridge.log("WARNING: Found Entrust IdentityGuard app but couldn't find a method to patch - this version of the app probably isn't supported yet");
+  }
 }
